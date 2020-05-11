f3441f4f767292ea728fc40e3784410e8abffe04
romqrq <rjaalbuquerque@gmail.com>
2020-05-11 14:15:03 +0000

    test

--- posts/views.py
+++ posts/views.py
@@ -11,7 +11,7 @@ def get_posts(request):
     """
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blogposts.html', {'posts': posts})
+    return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
