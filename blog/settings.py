bcb2b4670941dd99d9cd027e6f7a505816afd9e8
romqrq <rjaalbuquerque@gmail.com>
2020-05-11 14:50:47 +0000

    updated requirements to match source code

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
